50bb7f11b3a59ba0e6b9d29c6b21d5fa8285590f
Alex Rich <a.rich@salesforce.com>
2019-08-28 22:51:08 -0700

    add getters and setters for pixels

--- js/image.js
+++ js/image.js
@@ -3,11 +3,63 @@ export class MyImage {
         this.imageData = imageData;
     }
 
-    getWidth() {
+    get width() {
         return this.imageData.width;
     }
 
-    getHeight() {
+    get height() {
         return this.imageData.height;
+    }
+
+    get(x, y) {
+        const redIndex = _getRedIndex(x, y);
+        const r = this.imageData.data[redIndex];
+        const g = this.imageData.data[redIndex + 1];
+        const b = this.imageData.data[redIndex + 2];
+        const a = this.imageData.data[redIndex + 3];
+        return new Pixel(r, g, b, a);
+    }
+
+    /**
+     * set GrayScale
+     * sets the RGB value at (x, y) to v.
+     */
+    setG(x, y, v) {
+        set(x, y, v, v, v)
+    }
+
+    /**
+     * set Pixel
+     * Set's the pixel at (x, y) to p
+     */
+    setP(x, y, p) {
+        this.set(x, y, p.r, p.g, p.b, p.a);
+    }
+
+    set(x, y, r, g, b, a) {
+        const redIndex = _getRedIndex(x, y);
+        this.imageData.data[redIndex]     = r;
+        this.imageData.data[redIndex + 1] = g;
+        this.imageData.data[redIndex + 2] = b;
+        if (a) {
+            this.imageData.data[redIndex + 3] = a;
+        }
+    }
+
+    /**
+     * gets the index for the red value at x, y
+     * g, b, and a are +1, 2, & 3 respectively
+     */
+    _getRedIndex(x, y) {
+        return y * (this.width * 4) + x * 4;
+    }
+}
+
+export class Pixel {
+    constructor(r, g, b, a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
     }
 }
